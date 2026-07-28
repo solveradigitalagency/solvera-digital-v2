702c6e0376334925c0ff0d043098d2e6df578c26
CasaCarmona <lacasacarmona@gmail.com>
2026-07-27 22:39:52 -0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/shared/Footer";
 
+export const metadata = {
+  title: "Solvera Digital",
+  description: "...",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
+
 const manrope = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
